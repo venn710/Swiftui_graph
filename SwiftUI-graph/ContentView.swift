a97df68d7640c612c1aadad53eeddbfbd047ad98
venn <vennboddula@gmail.com>
2025-03-05 19:07:01 +0530

    Initial Commit

---
+++ SwiftUI-graph/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-graph
+//
+//  Created by Venkatesham Boddula on 05/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-graph/SwiftUI_graphApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_graphApp.swift
+//  SwiftUI-graph
+//
+//  Created by Venkatesham Boddula on 05/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_graphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
